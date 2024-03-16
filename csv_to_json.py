078b5b0938d3fca67fc64ba1a146975ee2855c63
Kamila Sproska <kamila.sproska@gmail.com>
2024-03-16 18:38:59 +0100

    added inserting movie script data to json files

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,13 +1,20 @@
 import csv
 import json
+import os.path
+from pprint import pprint
+from fuzzywuzzy import fuzz
 
 
-def csv_to_json(csv_file_path, json_file_path):
+def csv_to_json(csv_file_path, jsons_dir):
     # Read CSV file
     with open(csv_file_path, 'r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         # Convert CSV data to list of dictionaries
         data = list(csv_reader)
+
+    for row in data:
+        if 'Series_Title' in row:
+            row['Title'] = row.pop('Series_Title')
 
     for row in data:
         if 'IMDB_Rating' in row:
@@ -60,10 +67,38 @@ def csv_to_json(csv_file_path, json_file_path):
             del row[star_key]
         row['Stars'] = stars
 
+    filenames = [x.replace(".txt", "") for x in os.listdir('./scripts')]
+
+    movies_without_scripts = []
+    for row in data:
+        if os.path.exists(f"./scripts/{row['Title']}.txt"):
+            row['Script_path'] = f"{row['Title']}.txt"
+        elif os.path.exists(f"./scripts/{row['Title'].replace('The ', '')}.txt"):
+            row['Script_path'] = f"{row['Title'].replace('The ', '')}.txt"
+        else:
+            similarity_scores = [(string, fuzz.ratio(row['Title'], string)) for string in filenames]
+            sorted_list = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+            most_similar_string, similarity_score = sorted_list[0]
+            if similarity_score > 80:
+                row['Script_filename'] = f"{most_similar_string}.txt"
+            else:
+                movies_without_scripts.append(row['Title'])
+
+    print(len(movies_without_scripts), "/", len(data))
+    pprint(movies_without_scripts)
+    pprint(filenames)
+
     # Write JSON file
-    with open(json_file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+    for i, row in enumerate(data):
+        with_script = ""
+        if 'Script_filename' in row:
+            with_script = "_with_script"
+            with open(f"./scripts/{row['Script_filename']}", 'r') as script:
+                lines = script.readlines()
+                row['Script'] = "".join(lines).replace('"', '\"')
+        with open(jsons_dir + f'/movie_{i}{with_script}.json', 'w') as json_file:
+            json.dump(row, json_file, indent=4)
 
 
 # Example usage
-csv_to_json('imdb_top_1000.csv', 'imdb_top_1000.json')
+csv_to_json('imdb_top_1000.csv', './movie_data')
